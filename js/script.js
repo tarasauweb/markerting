46395d4219c95cad26a4f74a397cb1c2b71845a3
Artur Tarasau <tarasov.a.web@gmail.com>
2023-04-02 23:00:17 +0300

    feat:markerting section

---
+++ js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('load' , ()=>{
+    function loader() {
+        console.log(document.body.clientHeight)
+        const loader = document.querySelector(".loader");
+        const border = document.querySelector(".border");
+        const dot = ".";
+        let count = 0;
+        const timer = setInterval(() => {
+          count++;
+          loader.textContent += dot;
+          if (count > 3) {
+            loader.textContent = "";
+            clearInterval(timer);
+            count = 0;
+          }
+        }, 700);
+        setTimeout(() => {
+          border.classList.add("active-border");
+          // loader.style.display = "none";
+        }, 4500);
+        setTimeout(() => {
+          
+          loader.style.display = "none";
+        }, 5000);
+      }
+      loader()
+})
